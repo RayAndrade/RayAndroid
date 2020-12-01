f155339a5d96f770e3049c3b988618aaf4a3efb0
Ray Andrade <TheRayCode@RayAndrade.com>
2020-12-01 02:19:12 -0800

    youtu.be/aS__9RbCyHg to ActivityOne

--- ActivityDemo/app/src/main/java/com/theraycode/activitydemo/ActivityOne.java
+++ ActivityDemo/app/src/main/java/com/theraycode/activitydemo/ActivityOne.java
@@ -3,12 +3,47 @@ package com.theraycode.activitydemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class ActivityOne extends AppCompatActivity {
+
+    private String TAG="ActivityOne";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG,"onCreate");
         setContentView(R.layout.activity_one);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
     }
 }
